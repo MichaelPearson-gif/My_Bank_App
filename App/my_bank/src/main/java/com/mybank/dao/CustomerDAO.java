1f0e5586120a9e55c27b4cb33848f281974621ec
Michael Pearson <mpearson2516@yahoo.com>
2020-12-30 18:23:30 -0800

    Created a checked exception class and a connection to my db

--- App/my_bank/src/main/java/com/mybank/dao/CustomerDAO.java
+++ App/my_bank/src/main/java/com/mybank/dao/CustomerDAO.java
@@ -2,12 +2,13 @@ package com.mybank.dao;
 
 import java.util.List;
 
+import com.mybank.exception.BusinessException;
 import com.mybank.model.Customer;
 
 public interface CustomerDAO {
 
 	// Creates a new customer
-	public int createCustomer(Customer customer);
+	public int createCustomer(Customer customer) throws BusinessException;
 	
 	// Updates customer info
 	public Customer updateCustomer(int customerId);
--- App/my_bank/src/main/java/com/mybank/dao/impl/CustomerDAOImpl.java
+++ App/my_bank/src/main/java/com/mybank/dao/impl/CustomerDAOImpl.java
@@ -1,16 +1,26 @@
 package com.mybank.dao.impl;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.mybank.dao.CustomerDAO;
+import com.mybank.dao.dbutil.PostgresqlConnection;
+import com.mybank.exception.BusinessException;
 import com.mybank.model.Customer;
 
 public class CustomerDAOImpl implements CustomerDAO{
 
 	@Override
-	public int createCustomer(Customer customer) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int createCustomer(Customer customer) throws BusinessException {
+		int c = 0;
+		try (Connection connection = PostgresqlConnection.getConnection()){
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			throw new BusinessException("Internal error occured contact System Admin");
+		}
+
+		return c;
 	}
 
 	@Override
