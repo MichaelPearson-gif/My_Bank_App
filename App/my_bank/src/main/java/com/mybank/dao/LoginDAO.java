87cc6f9df300133a7623425f8be86e6a90091f39
Michael Pearson <mpearson2516@yahoo.com>
2021-01-07 07:03:46 -0800

    working on login dao implementation

--- App/my_bank/src/main/java/com/mybank/dao/LoginDAO.java
+++ App/my_bank/src/main/java/com/mybank/dao/LoginDAO.java
@@ -1,11 +1,12 @@
 package com.mybank.dao;
 
+import com.mybank.exception.BusinessException;
 import com.mybank.model.Login;
 
 public interface LoginDAO {
 	
 	// Customer creates creates a login method
-	public int createLogin(Login login);
+	public int createLogin(Login login) throws BusinessException;
 	
 	// Customer can update their username or password
 	public Login updateLogin(int customerId);
--- App/my_bank/src/main/java/com/mybank/dao/impl/LoginDAOImpl.java
+++ App/my_bank/src/main/java/com/mybank/dao/impl/LoginDAOImpl.java
@@ -1,14 +1,36 @@
 package com.mybank.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import com.mybank.dao.LoginDAO;
+import com.mybank.dao.dbutil.PostgresqlConnection;
+import com.mybank.exception.BusinessException;
 import com.mybank.model.Login;
 
 public class LoginDAOImpl implements LoginDAO {
 
 	@Override
-	public int createLogin(Login login) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int createLogin(Login login) throws BusinessException {
+		int c = 0;
+		
+		try (Connection connection = PostgresqlConnection.getConnection()){
+			
+			String sql = "INSERT INTO bank.login(username, password, customer_id) VALUES(?, ?, ?)";
+			
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, login.getUsername());
+			preparedStatement.setString(2, login.getPassword());
+			preparedStatement.setInt(3, login.getCustomerId());
+			
+			c = preparedStatement.executeUpdate();
+			
+		}catch (ClassNotFoundException | SQLException e) {
+			throw new BusinessException("Internal error occured contact System Admin");
+		}
+		
+		return c;
 	}
 
 	@Override
