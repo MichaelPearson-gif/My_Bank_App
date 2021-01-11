73fa8fab6e18cf0dc0ccbe85c2b6ab61e3e0baef
Michael Pearson <mpearson2516@yahoo.com>
2021-01-10 16:47:19 -0800

    Wrote psuedo code for the BankMain

---
+++ App/my_bank/src/main/java/com/mybank/main/BankMain.java
@@ -0,0 +1,37 @@
+package com.mybank.main;
+
+import java.util.Scanner;
+
+import com.mybank.service.BankService;
+import com.mybank.service.impl.BankServiceImpl;
+
+public class BankMain {
+
+	public static void main(String[] args) {
+		// Create an instance of the service layer
+		BankService bankService = new BankServiceImpl();
+		
+		// Scanner is used to take in user inputs
+		Scanner sc = new Scanner(System.in);
+		
+		// Print Welcome message
+		// Create a new blank line
+		
+		// Create a variable for the switch cases
+		
+		// Do While  for the switch cases
+			// Under the do:
+				// Print instructions to the user
+		
+				// Add a try catch block to parse through the switch case variable
+		
+				// Create switch cases
+				// Last switch case is the default case
+		
+			// Set the while condition
+		
+		
+
+	}
+
+}
