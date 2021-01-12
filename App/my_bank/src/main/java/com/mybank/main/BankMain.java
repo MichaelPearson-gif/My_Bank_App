5da55178b4c02e242b56863f96079a352d7d68b8
Michael Pearson <mpearson2516@yahoo.com>
2021-01-11 20:44:56 -0800

    Created first menu option, need to fix the sc.nextInt

--- App/my_bank/src/main/java/com/mybank/main/BankMain.java
+++ App/my_bank/src/main/java/com/mybank/main/BankMain.java
@@ -1,9 +1,14 @@
 package com.mybank.main;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 import org.apache.log4j.Logger;
 
+import com.mybank.exception.BusinessException;
+import com.mybank.model.User;
 import com.mybank.service.BankService;
 import com.mybank.service.impl.BankServiceImpl;
 
@@ -12,12 +17,17 @@ public class BankMain {
 	// Create a logger variable
 	private static Logger log = Logger.getLogger(BankMain.class);
 	
-	public static void main(String[] args) {
-		// Create an instance of the service layer
-		BankService bankService = new BankServiceImpl();
-		
-		// Scanner is used to take in user inputs
-		Scanner sc = new Scanner(System.in);
+	// Create an instance of the service layer
+	private static BankService bankService = new BankServiceImpl();
+	
+	// Create an instance of the User class
+	User user = new User();
+	
+	// Scanner is used to take in user inputs
+	// Scanner is static so I can reference it in my static methods
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) throws BusinessException {
 		
 		// Print Welcome message
 		log.info("Welcome to MyBank application V1.00");
@@ -48,7 +58,7 @@ public class BankMain {
 			switch(ch) {
 			
 			case 1:
-				log.info("This option is still under developement");
+				Register();
 				break;
 				
 			case 2:
@@ -65,6 +75,68 @@ public class BankMain {
 			}
 			
 		}while(ch != 3);
+		
+	}
+	
+	public static void Register() throws BusinessException {
+		User user = new User();
+		
+		// Create the simple date format to help convert a string type to a date type
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+		sdf.setLenient(false);
+		Date date = null;
+		
+		// Getting User info
+		log.info("Please fill out the info below");
+		log.info("");
+		log.info("User Id");
+		String userId = sc.nextLine();
+		log.info("");
+		log.info("First Name");
+		String firstName = sc.nextLine();
+		log.info("");
+		log.info("Last Name");
+		String lastName = sc.nextLine();
+		log.info("");
+		log.info("Driver's License");
+		String driversLicense = sc.nextLine();
+		log.info("");
+		log.info("Social Security Number");
+		Integer ssn = sc.nextInt();
+		log.info("");
+		log.info("Email");
+		String email = sc.nextLine();
+		log.info("");
+		log.info("Billing Address");
+		String billingAddress = sc.nextLine();
+		log.info("");
+		log.info("Date of Birth (yyyy-mm-dd)");
+		String dob = sc.nextLine();
+		log.info("");
+		log.info("Gender (M or F)");
+		String gender = sc.nextLine();
+		log.info("");
+		log.info("password");
+		String password = sc.nextLine();
+		
+		// Convert the dob from a String to a Date format and create the new user
+		try {
+			date = sdf.parse(dob);
+			
+			user.setUserId(userId);
+			user.setFirstName(firstName);
+			user.setLastName(lastName);
+			user.setDriversLicense(driversLicense);
+			user.setSsn(ssn);
+			user.setEmail(email);
+			user.setBillingAddress(billingAddress);
+			user.setDob(date);
+			user.setGender(gender);
+			user.setPassword(password);
+			bankService.createUser(user);
+		}catch(ParseException e) {
+			log.info("Invalid Date");
+		}
 		
 	}
 
